7842848fd4ac3bfcb1954b1c420de19f79b542b1
ray <ray@gmail.com>
2024-04-28 16:37:21 +0800

    11

--- deploy/supervisor.d/executor-srv.sh
+++ deploy/supervisor.d/executor-srv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /data/ragflow/ragflow
 export PYTHONPATH=/usr/local/lib64/python3.9/site-packages:/usr/local/lib64/python3.9/site-packages:.
-pythono3 rag/svr/task_executor.py
+python3 rag/svr/task_executor.py
